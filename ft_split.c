3736f5f59dab88879ccd29fe36038b566793b567
Pedro jesus Gomez ruiz <pgomez-r@c2r10s4.42malaga.com>
2022-10-12 18:41:51 +0200

    12_10

--- ft_split.c
+++ ft_split.c
@@ -6,18 +6,17 @@
 /*   By: pgomez-r <pgomez-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 13:34:13 by pgomez-r          #+#    #+#             */
-/*   Updated: 2022/10/11 18:05:47 by pgomez-r         ###   ########.fr       */
+/*   Updated: 2022/10/12 18:39:43 by pgomez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 /*La función asigna memoria y devuelve una array de strings terminados en '\0'
 que se genera a partir de una string que recibe como parámetro
-Esa cadena origen la queremos separar en palabras, en otras cadenas, split tiene
-que econtrar y separar cada palabra, dejando de lado los espacios y caracteres 
-especiales
-La vamos a dividir, 1º un contador para saber cuantas palabras (cadenas) 
-tenemos que meter en el array final que devuelve la función*/
+Esa cadena origen la queremos separar en otras cadenas (palabras), split tiene
+que econtrar y separar cada palabra, dejando de lado el caracter dado por 
+el parámetro char c*/
+
+#include "libft.h"
 
 int	cont_word(char const *s, char c)
 {
@@ -38,15 +37,57 @@ int	cont_word(char const *s, char c)
 	return (count);
 }
 
-/*Qué hace el contador = un ciclo con tres "checks" dentro
+/*Qué hace el contador = un ciclo con tres "checks" dentro:
 while(recorrer toda la cadena original *s)
 	while(si lo que hay en cada posicion de str == c, seguimos i++)
 	if(terminado el while de arriba, porque str != c, aumentamos cntr+ 1 vez)
-	while(si no ha terminado str y s[i] es distinto a c, seguimos i++)*/
+	while(si no ha terminado str y s[i] es distinto a c, seguimos i++)
+Así ya tenemos el número de strings que va a tener nuestra tabla resultado*/
 
-/*OK, ya tenemos el número de strings que va a tener nuestra tabla resultado
-Ahora, */
+char	*ft_savewords(const char *s, size_t n)
+{
+	char			*str;
+
+	str = (char *)malloc(sizeof(char) * n + 1);
+	if (str == NULL)
+		return (NULL);
+	str = ft_strncpy(str, s, n);
+	str[n] = '\0';
+	return (str);
+}
+
+/*Qué hace saveword = guardar como char* cada palabra que ft_split "encuentra"
+Asigna memoria para el nuevo string con malloc y tamaño de n, que también ha 
+calculado y enviado ft_split
+Usa strncpy para copiar la cadena que llega desde ft_split hasta la posicion n
+y así crea la nueva, solo con las posiciones de la original que nos interesa
+Cómo = fr_split usa 3 contadores que nos dicen en que posicion de la original, 
+hasta que posicion copiar y en que posicion de la tabla nueva colocar esto*/
 char	**ft_split(char const *s, char c)
 {
-	
+	int				i;
+	int				j;
+	int				k;
+	char			**tab;
+
+	i = 0;
+	k = 0;
+	tab = (char **)malloc(sizeof(char *) * (ft_cntwrd(s, c)) + 1);
+	if (tab == NULL)
+		return (NULL);
+	while (s[i])
+	{
+		while (s[i] == c)
+			i++;
+		j = i;
+		while (s[i] && s[1] != c)
+			i++;
+		if (i > j)
+		{	
+			tab[k] = ft_savewords(s[j], i - j);
+			k++;
+		}
+	}
+	tab[k] = '\0';
+	return (tab);
 }
--- main_prueba.c
+++ main_prueba.c
@@ -6,70 +6,123 @@
 /*   By: pgomez-r <pgomez-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 11:43:23 by pgomez-r          #+#    #+#             */
-/*   Updated: 2022/10/10 13:00:55 by pgomez-r         ###   ########.fr       */
+/*   Updated: 2022/10/12 16:35:21 by pgomez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 
-int	ft_strlen(const char *str)
+int	cnt_words(char const *s, char c)
+
 {
-	int	cont;
-	int	num_char;
+	int	i;
+	int	cnt;
+	int	wrd;
 
-	cont = 0;
-	num_char = 0;
-	while (str[cont] != '\0')
-	{
-		num_char++;
-		cont++;
-	}
-	return (num_char);
-}
-
-static int	ft_char_in_set(char c, char const *set)
-{
-	size_t	i;
-
+	cnt = 0;
 	i = 0;
-	while (set[i])
+	wrd = 0;
+	while (s[i])
 	{
-		if (set[i] == c)
-			return (1);
+		if ((s[i] != c) && (wrd == 0))
+		{
+			wrd = 1;
+			cnt += 1;
+		}
+		else if (s[i] == c)
+			wrd = 0;
 		i++;
 	}
+	return (cnt);
+}
+
+int	len_words(char *s, int *i, char c)
+{
+	int	x;
+	int	y;
+
+	x = 0;
+	while ((s[*i] == c) && s[*i])
+	{
+		*i += 1;
+	}
+	y = *i;
+	while (s[y] && (s[y] != c))
+	{
+		y++;
+		x++;
+	}
+	return (x);
+}
+
+int	wrt_words(char *s, char **p, int *i, char c)
+{
+	int		wrd_sz;
+	int		j;
+	int		ip;
+	char	*pw;
+
+	wrd_sz = len_words(s, i, c);
+	ip = 0;
+	while (wrd_sz != 0)
+	{
+		pw = (char *)calloc(sizeof(char), (wrd_sz + 1));
+		if (!pw)
+			return (1);
+		j = 0;
+		while (j < wrd_sz)
+		{
+			pw[j++] = s[*i];
+			*i += 1;
+		}
+		pw[j] = '\0';
+		p[ip] = pw;
+		ip++;
+		wrd_sz = len_words(s, i, c);
+	}
+	p[ip] = NULL;
 	return (0);
 }
 
-char	*ft_strtrim(char const *s1, char const *set)
+char	**ft_split(char const *s, char c)
 {
-	char	*str;
-	size_t	i;
-	size_t	start;
-	size_t	end;
+	int		count;
+	char	**result;
+	int		i;
+	int		error;
 
-	start = 0;
-	while (s1[start] && ft_char_in_set(s1[start], set))
-		start++;
-	end = ft_strlen(s1);
-	while (end > start && ft_char_in_set(s1[end - 1], set))
-		end--;
-	str = (char *)malloc(sizeof(*s1) * (end - start + 1));
-	if (!str)
+	if (!s)
+		return (NULL);
+	count = cnt_words(s, c);
+	result = (char **)calloc(sizeof(char *), count + 1);
+	if (!result)
 		return (NULL);
 	i = 0;
-	while (start < end)
-		str[i++] = s1[start++];
-	str[i] = 0;
-	return (str);
+	error = wrt_words((char *)s, result, &i, c);
+	if (error)
+	{
+		i = 0;
+		while (result[i])
+		{
+			free(result[i]);
+			i++;
+		}
+		free(result);
+		return (NULL);
+	}
+	return (result);
 }
 
 int	main(void)
 {
-	char	*nueva;
-
-	nueva = ft_strtrim("cadena nueva de cadena", "cadena");
-	printf("%s\n", nueva);
+	char const *ch = "hola, que tal?";
+	char c = ' ';
+	int i = 0;
+	while (i < cnt_words(ch, c))
+	{
+		printf("%s\n", ft_split(ch, c)[i]);
+		i++;
+	}
+	return (0);
 }
