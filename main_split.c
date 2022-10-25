ccef67f01a459a70d8f41f78ad695e087d253889
Pedro jesus Gomez ruiz <pgomez-r@c3r10s3.42malaga.com>
2022-10-25 13:58:17 +0200

    26_10_debugger

--- main_split.c
+++ main_split.c
@@ -6,96 +6,96 @@
 /*   By: pgomez-r <pgomez-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 19:13:51 by pgomez-r          #+#    #+#             */
-/*   Updated: 2022/10/21 16:26:51 by pgomez-r         ###   ########.fr       */
+/*   Updated: 2022/10/25 13:05:19 by pgomez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "libft.h"
+#include "libft.h"
 
-// char	*ft_strncpy(char *dest, const char *src, unsigned int n)
-// {
-// 	unsigned int	cnt;
+char	*ft_strncpy(char *dest, const char *src, unsigned int n)
+{
+	unsigned int	cnt;
 
-// 	cnt = 0;
-// 	while (cnt < n)
-// 	{
-// 		dest[cnt] = src[cnt];
-// 		cnt++;
-// 	}
-// 	dest[cnt] = '\0';
-// 	return (dest);
-// }
+	cnt = 0;
+	while (cnt < n)
+	{
+		dest[cnt] = src[cnt];
+		cnt++;
+	}
+	dest[cnt] = '\0';
+	return (dest);
+}
 
-// int	ft_count_word(char const *s, char c)
-// {
-// 	unsigned int	i;
-// 	int				count;
+int	ft_count_word(char const *s, char c)
+{
+	unsigned int	i;
+	int				count;
 
-// 	i = 0;
-// 	count = 0;
-// 	while (s[i] != '\0')
-// 	{
-// 		while (s[i] == c)
-// 			i++;
-// 		if (s[i] != '\0')
-// 			count++;
-// 		while ((s[i] != '\0') && (s[i] != c))
-// 			i++;
-// 	}
-// 	return (count);
-// }
+	i = 0;
+	count = 0;
+	while (s[i] != '\0')
+	{
+		while (s[i] == c)
+			i++;
+		if (s[i] != '\0')
+			count++;
+		while ((s[i] != '\0') && (s[i] != c))
+			i++;
+	}
+	return (count);
+}
 
-// char	*ft_savewords(const char *s, unsigned int n)
-// {
-// 	char			*str;
+char	*ft_savewords(const char *s, unsigned int n)
+{
+	char			*str;
 
-// 	str = (char *)malloc(sizeof(char) * n + 1);
-// 	if (str == NULL)
-// 		return (NULL);
-// 	str = ft_strncpy(str, s, n);
-// 	str[n] = '\0';
-// 	return (str);
-// }
+	str = (char *)malloc(sizeof(char) * n + 1);
+	if (str == NULL)
+		return (NULL);
+	str = ft_strncpy(str, s, n);
+	str[n] = '\0';
+	return (str);
+}
 
-// char	**ft_split(char const *s, char c)
-// {
-// 	int				i;
-// 	int				j;
-// 	int				k;
-// 	char			**tab;
+char	**ft_split(char const *s, char c)
+{
+	int				i;
+	int				j;
+	int				k;
+	char			**tab;
 
-// 	i = 0;
-// 	k = 0;
-// 	tab = (char **)malloc(sizeof(char *) * (ft_count_word(s, c)) + 1);
-// 	if (!tab || !s)
-// 		return (NULL);
-// 	while (s[i])
-// 	{
-// 		while (s[i] == c)
-// 			i++;
-// 		j = i;
-// 		while (s[i] && s[i] != c)
-// 			i++;
-// 		if (i > j)
-// 		{	
-// 			tab[k] = ft_savewords(s + j, i - j);
-// 			k++;
-// 		}
-// 	}
-// 	tab[k] = NULL;
-// 	return (tab);
-// }
+	i = 0;
+	k = 0;
+	tab = (char **)malloc(sizeof(char *) * (ft_count_word(s, c)) + 1);
+	if (!tab || !s)
+		return (NULL);
+	while (s[i])
+	{
+		while (s[i] == c)
+			i++;
+		j = i;
+		while (s[i] && s[i] != c)
+			i++;
+		if (i > j)
+		{	
+			tab[k] = ft_savewords(s + j, i - j);
+			k++;
+		}
+	}
+	tab[k] = NULL;
+	return (tab);
+}
 
-// int	main(void)
-// {
-// 	char const *ch = "olol                     ";
-// 	char c = ' ';
-// 	int i = 0;
-// 	while (i < ft_count_word(ch, c))
-// 	{
-// 		// printf("%s\n", split(ch, c)[i]);
-// 		printf("%s\n", ft_split(ch, c)[i]);
-// 		i++;
-// 	}
-// 	return (0);
-// }
+int	main(void)
+{
+	char const *ch = "olol                     ";
+	char c = ' ';
+	int i = 0;
+	while (i < ft_count_word(ch, c))
+	{
+		// printf("%s\n", split(ch, c)[i]);
+		printf("%s\n", ft_split(ch, c)[i]);
+		i++;
+	}
+	return (0);
+}
