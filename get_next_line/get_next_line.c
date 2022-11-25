7b2a2b5b32e1d1e8ea4ebceaa58f8a4d505c9e40
Pedro jesus Gomez ruiz <pgomez-r@c1r15s6.42malaga.com>
2022-11-25 18:13:58 +0100

    25_11_22

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,16 +6,25 @@
 /*   By: pgomez-r <pgomez-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 15:25:40 by pgomez-r          #+#    #+#             */
-/*   Updated: 2022/11/24 15:30:36 by pgomez-r         ###   ########.fr       */
+/*   Updated: 2022/11/24 16:08:25 by pgomez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
 char	*get_next_line(int fd)
 {
-	
+	static char	*line;
+
+	line = "Holamundo\n";
 }
 
-int main(void)
+int	main(void)
 {
-	
+	int	fd;
+
+	printf(get_next_line(fd));
+	printf(get_next_line(fd));
+	printf(get_next_line(fd));
+	printf(get_next_line(fd));
 }
