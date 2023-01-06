d36bbd7a124da8699030645d42c36fd1bc8d41da
pgomez-r <pedrogruz.11@gmail.com>
2023-01-06 17:22:01 +0100

    06_01_23

--- push_swap/draft/main.c
+++ push_swap/draft/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pgomez-r <pgomez-r@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pgruz <pgruz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 17:23:25 by pgomez-r          #+#    #+#             */
-/*   Updated: 2023/01/05 16:25:30 by pgomez-r         ###   ########.fr       */
+/*   Updated: 2023/01/06 13:24:50 by pgruz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,24 +21,32 @@ int	main(int ac, char **av)
 {
 	char	**numbers;
 	size_t	i;
-	int		*array;
+	int		*array_a;
+	int		*array_b;
 
 	atexit(ft_leaks);
 	numbers = ft_argtochar(ac, av);
 	if (!numbers)
 		return (printf("Error"), -1);
-	array = ft_getarray(numbers);
-	if (!ft_chksort(array, ft_arrlen(array)))
-		return (ft_totalfree(numbers), free(array), printf("Error"), -1);
+	array_a = ft_getarray(numbers);
+	if (!ft_chksort(array_a, ft_arrlen(array_a)))
+		return (ft_totalfree(numbers), free(array_a), printf("Error"), -1);
+	array_b = malloc(sizeof(int) * ft_arrlen(array_a));
+	i = 0;
+	while (i < ft_arrlen(array_a))
+	{
+		array_b[i] = 0;
+		i++;
+	}
 	i = 0;
 	while (numbers[i])
 		printf ("%s ", numbers[i++]);
 	printf ("CHAR STRING\n");
 	i = 0;
-	while (i < ft_arrlen(array))
-		printf ("%i ", array[i++]);
+	while (i < ft_arrlen(array_a))
+		printf ("%i ", array_a[i++]);
 	printf ("INT ARRAY\n");
 	ft_totalfree(numbers);
-	free(array);
+	free(array_a);
 	return (0);
 }
---
+++ push_swap/draft/rev_operations.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rev_operations.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pgruz <pgruz@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/06 12:35:44 by pgruz             #+#    #+#             */
+/*   Updated: 2023/01/06 12:37:32 by pgruz            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	rotate_a(int *array_a, int *array_b)
+{
+	
+}
+
+void	rotate_b(int *array_a, int *array_b)
+{
+	
+}
+
+void	revrot_a(int *array_a, int *array_b)
+{
+	
+}
+
+void	revrot_b(int *array_a, int *array_b)
+{
+	
+}
