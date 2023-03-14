722b3012c89105f9542d4e3fd38bf5613c39ab25
Pedro jesus Gomez ruiz <pgomez-r@c3r10s5.42malaga.com>
2023-03-14 23:39:51 +0100

    14-03-23

---
+++ pipex/pipex.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pgomez-r <pgomez-r@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/14 22:01:13 by pgomez-r          #+#    #+#             */
+/*   Updated: 2023/03/14 22:02:26 by pgomez-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h> 
+# include <unistd.h>
+# include <stdarg.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <limits.h>
+
+/*structs*/
+
+/*main.c*/
+
+#endif
