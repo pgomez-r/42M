929a88fec287ba89c4dd9365bc41524d848030ce
Pedro jesus Gomez ruiz <pgomez-r@c3r10s1.42malaga.com>
2023-05-11 18:22:25 +0200

    11-05-23

--- so_long/incl/so_long.h
+++ so_long/incl/so_long.h
@@ -6,7 +6,7 @@
 /*   By: pgomez-r <pgomez-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 21:51:16 by pgomez-r          #+#    #+#             */
-/*   Updated: 2023/05/10 18:43:46 by pgomez-r         ###   ########.fr       */
+/*   Updated: 2023/05/11 17:10:18 by pgomez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include "./libft42/libft.h"
 # include "./MLX42/include/MLX42/MLX42.h"
 # include "./MLX42/include/MLX42/MLX42_Input.h"
+
 /*structs*/
 
 #endif
--- so_long/src/main.c
+++ so_long/src/main.c
@@ -6,17 +6,58 @@
 /*   By: pgomez-r <pgomez-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 18:36:09 by pgomez-r          #+#    #+#             */
-/*   Updated: 2023/05/10 22:53:22 by pgomez-r         ###   ########.fr       */
+/*   Updated: 2023/05/11 18:15:05 by pgomez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incl/so_long.h"
 
-int	main(void)
+static mlx_image_t		*g_image;
+
+void	ft_paintsquare(uint32_t color)
+{
+	int32_t		x;
+	int32_t		y;
+
+	x = 0;
+	while (x < (int32_t)g_image->width)
+	{
+		y = 0;
+		while (y < (int32_t)g_image->height)
+		{
+			mlx_put_pixel(g_image, x, y, color);
+			y++;
+		}
+		x++;
+	}
+}
+
+void	ft_hook(void *param)
 {
 	mlx_t	*mlx;
 
-	mlx = mlx_init(860, 640, "42", true);
+	mlx = param;
+	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
+		mlx_close_window(mlx);
+	if (mlx_is_key_down(mlx, MLX_KEY_UP))
+		g_image->instances[0].y -= 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
+		g_image->instances[0].y += 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
+		g_image->instances[0].x -= 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
+		g_image->instances[0].x += 5;
+}
+
+int	main(void)
+{
+	mlx_t					*mlx;
+
+	mlx = mlx_init(860, 640, "42", false);
+	g_image = mlx_new_image(mlx, 128, 128);
+	mlx_image_to_window(mlx, g_image, 0, 0);
+	ft_paintsquare(0x006400fff);
+	mlx_loop_hook(mlx, ft_hook, mlx);
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
 	printf("Has cerrado la ventanada, shulo\n");
