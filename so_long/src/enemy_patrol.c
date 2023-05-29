c2837f60b43f4116a03788df5caa65488ff0dd27
Pedro jesus Gomez ruiz <pgomez-r@c3r9s1.42malaga.com>
2023-05-29 13:07:03 +0200

    29-05-23

--- so_long/src/enemy_patrol.c
+++ so_long/src/enemy_patrol.c
@@ -6,7 +6,7 @@
 /*   By: pgomez-r <pgomez-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 08:34:46 by pgomez-r          #+#    #+#             */
-/*   Updated: 2023/05/29 12:38:31 by pgomez-r         ###   ########.fr       */
+/*   Updated: 2023/05/29 12:57:15 by pgomez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,53 +57,53 @@ void	enemy_patrol(t_struct *st)
 	dir = ft_maxvalue_pos(movs, 4);
 	if (dir == 0)
 	{
-		if (st->flag == 0 && st->map[(st->enemy_d->instances[4].y - 2) / PIX][x / PIX] != '1'
-			&& st->map[(st->enemy_d->instances[4].y - 2) / PIX][x / PIX] != 'X')
-			st->enemy_d->instances[4].y -= 2;
+		if (st->flag == 0 && st->map[(st->enemy_d->instances[0].y - 2) / PIX][x / PIX] != '1'
+			&& st->map[(st->enemy_d->instances[0].y - 2) / PIX][x / PIX] != 'X')
+			st->enemy_d->instances[0].y -= 2;
 		else
 			st->flag = 1;
-		if (st->flag == 1 && st->map[(st->enemy_d->instances[4].y + 66) / PIX][x / PIX] != '1'
-			&& st->map[(st->enemy_d->instances[4].y + 66) / PIX][x / PIX] != 'X')
-			st->enemy_d->instances[4].y += 2;
+		if (st->flag == 1 && st->map[(st->enemy_d->instances[0].y + 66) / PIX][x / PIX] != '1'
+			&& st->map[(st->enemy_d->instances[0].y + 66) / PIX][x / PIX] != 'X')
+			st->enemy_d->instances[0].y += 2;
 		else
 			st->flag = 0;
 	}
 	else if (dir == 1)
 	{
-		if (st->flag == 0 && st->map[(st->enemy_d->instances[4].y + 66) / PIX][x / PIX] != '1'
-			&& st->map[(st->enemy_d->instances[4].y + 66) / PIX][x / PIX] != 'X')
-			st->enemy_d->instances[4].y += 2;
+		if (st->flag == 0 && st->map[(st->enemy_d->instances[0].y + 66) / PIX][x / PIX] != '1'
+			&& st->map[(st->enemy_d->instances[0].y + 66) / PIX][x / PIX] != 'X')
+			st->enemy_d->instances[0].y += 2;
 		else
 			st->flag = 1;
-		if (st->flag == 1 && st->map[(st->enemy_d->instances[4].y - 2) / PIX][x / PIX] != '1'
-			&& st->map[(st->enemy_d->instances[4].y - 2) / PIX][x / PIX] != 'X')
-			st->enemy_d->instances[4].y -= 2;
+		if (st->flag == 1 && st->map[(st->enemy_d->instances[0].y - 2) / PIX][x / PIX] != '1'
+			&& st->map[(st->enemy_d->instances[0].y - 2) / PIX][x / PIX] != 'X')
+			st->enemy_d->instances[0].y -= 2;
 		else
 			st->flag = 0;
 	}
 	else if (dir == 2)
 	{
-		if (st->flag == 0 && st->map[y / PIX][(st->enemy_d->instances[4].x - 2) / PIX] != '1'
-			&& st->map[y / PIX][(st->enemy_d->instances[4].x - 2) / PIX] != 'X')
-			st->enemy_d->instances[4].x -= 2;
+		if (st->flag == 0 && st->map[y / PIX][(st->enemy_d->instances[0].x - 2) / PIX] != '1'
+			&& st->map[y / PIX][(st->enemy_d->instances[0].x - 2) / PIX] != 'X')
+			st->enemy_d->instances[0].x -= 2;
 		else
 			st->flag = 1;
-		if (st->flag == 1 && st->map[y / PIX][(st->enemy_d->instances[4].x + 66) / PIX] != '1'
-			&& st->map[y / PIX][(st->enemy_d->instances[4].x + 66) / PIX] != 'X')
-			st->enemy_d->instances[4].x += 2;
+		if (st->flag == 1 && st->map[y / PIX][(st->enemy_d->instances[0].x + 66) / PIX] != '1'
+			&& st->map[y / PIX][(st->enemy_d->instances[0].x + 66) / PIX] != 'X')
+			st->enemy_d->instances[0].x += 2;
 		else
 			st->flag = 0;
 	}
 	else if (dir == 3)
 	{
-		if (st->flag == 0 && st->map[y / PIX][(st->enemy_d->instances[4].x + 66) / PIX] != '1'
-			&& st->map[y / PIX][(st->enemy_d->instances[4].x + 66) / PIX] != 'X')
-			st->enemy_d->instances[4].x += 2;
+		if (st->flag == 0 && st->map[y / PIX][(st->enemy_d->instances[0].x + 66) / PIX] != '1'
+			&& st->map[y / PIX][(st->enemy_d->instances[0].x + 66) / PIX] != 'X')
+			st->enemy_d->instances[0].x += 2;
 		else
 			st->flag = 1;
-		if (st->flag == 1 && st->map[y / PIX][(st->enemy_d->instances[4].x - 2) / PIX] != '1'
-			&& st->map[y / PIX][(st->enemy_d->instances[4].x - 2) / PIX] != 'X')
-			st->enemy_d->instances[4].x -= 2;
+		if (st->flag == 1 && st->map[y / PIX][(st->enemy_d->instances[0].x - 2) / PIX] != '1'
+			&& st->map[y / PIX][(st->enemy_d->instances[0].x - 2) / PIX] != 'X')
+			st->enemy_d->instances[0].x -= 2;
 		else
 			st->flag = 0;
 	}
