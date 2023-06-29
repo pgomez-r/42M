cb5a7405fdfddbf4ad7e62479f08d0ae61a7c226
Pedro jesus Gomez ruiz <pgomez-r@c3r1s2.42malaga.com>
2023-06-29 20:27:29 +0200

    29-06-23

--- exam_rank02/level_4/flood_fill.c
+++ exam_rank02/level_4/flood_fill.c
@@ -6,18 +6,41 @@
 /*   By: pgomez-r <pgomez-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 21:15:18 by pgomez-r          #+#    #+#             */
-/*   Updated: 2023/06/28 22:03:43 by pgomez-r         ###   ########.fr       */
+/*   Updated: 2023/06/29 18:40:35 by pgomez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "flood_fill.h"
 
+void	fill(char **tab, t_point size, t_point begin, char c)
+{
+	int	x;
+	int	y;
+
+	x = begin->x;
+	y = begin->y;
+	if ()
+}
 void  flood_fill(char **tab, t_point size, t_point begin)
 {
-	
+	fill(tab, size, begin, tab[begin.y][begin.x]);
 }
 
 int	main(void)
 {
-	
+	t_point		size = {8, 5};
+	t_point		begin = {1, 1};
+	char		*zone[] = {
+		"11111111",
+		"10001001",
+		"10010001",
+		"10110001",
+		"11100001",
+	};
+	int			i = -1;
+
+	while (zone[++i] != NULL)
+		printf("%s\n");
+	printf("\n");
+	flood_fill(zone, size, begin);
 }
