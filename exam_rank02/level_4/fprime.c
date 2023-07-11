67e21b1f74cb815bee7e1fb55baaf73fb41e297a
pgomez-r <pedrogruz.11@gmail.com>
2023-07-11 20:07:21 +0200

    11-07-23

--- exam_rank02/level_4/fprime.c
+++ exam_rank02/level_4/fprime.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fprime.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pgomez-r <pgomez-r@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pgruz <pgruz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/30 19:11:04 by pgomez-r          #+#    #+#             */
-/*   Updated: 2023/06/30 19:49:58 by pgomez-r         ###   ########.fr       */
+/*   Updated: 2023/07/11 20:06:01 by pgruz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int	ft_isprime(int num)
+{
+	int	i;
+
+	i = 1;
+	if (num == 2)
+		return (0);
+	while (++i < num)
+	{
+		if (num % i == 0)
+			return (-1);
+	}
+	return (0);
+}
+
 int	main(int ac, char **av)
 {
 	int	num;
 	int	i;
+	int	flag;
 
 	if (ac != 2)
 		return (printf("\n"), 0);
 	num = atoi(av[1]);
-	if (num == 1)
-		return (printf("1\n"), 0);
+	// if (num == 1)
+	// 	return (printf("1\n"), 0);
+	flag = 0;
 	i = 1;
-	while (num >= ++i)
+	while (++i <= num)
 	{
-		
+		if (num % i == 0 && ft_isprime(num) == 0 && flag == 0)
+		{	
+			printf("%d", i);
+			num = num / i;
+			flag++;
+		}
+		else if (num % i == 0 && ft_isprime(num) == 0)
+		{	
+			printf("*%d", i);
+			num = num / i;
+		}
 	}
-	return (0);
+	return (printf("\n"), 0);
 }
+
+/*Según el subject, el input (los argumentos que recibe el programa) es
+siempre válido, así que no tenemos que contemplar la validación o parseo de 
+argv, pero si protegemos el caso en el que se recibe más o menos de un 
+argumento, en ese caso imprimimos salto de línea*/
+
+/*Ejemplo) La factorización prima de 96 (2 • 2 • 2 • 2 • 2 • 3)*/
