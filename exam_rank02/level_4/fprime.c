519ca1608c926bf8438e8d6db8e613fe95e4bee8
pgomez-r <pedrogruz.11@gmail.com>
2023-07-13 20:59:26 +0200

    13-06-23

--- exam_rank02/level_4/fprime.c
+++ exam_rank02/level_4/fprime.c
@@ -6,7 +6,7 @@
 /*   By: pgruz <pgruz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/30 19:11:04 by pgomez-r          #+#    #+#             */
-/*   Updated: 2023/07/11 20:06:01 by pgruz            ###   ########.fr       */
+/*   Updated: 2023/07/13 20:59:04 by pgruz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,21 +39,19 @@ int	main(int ac, char **av)
 	if (ac != 2)
 		return (printf("\n"), 0);
 	num = atoi(av[1]);
-	// if (num == 1)
-	// 	return (printf("1\n"), 0);
 	flag = 0;
 	i = 1;
 	while (++i <= num)
 	{
-		if (num % i == 0 && ft_isprime(num) == 0 && flag == 0)
+		while (num % i == 0 && !ft_isprime(i))
 		{	
-			printf("%d", i);
-			num = num / i;
-			flag++;
-		}
-		else if (num % i == 0 && ft_isprime(num) == 0)
-		{	
-			printf("*%d", i);
+			if (!flag)
+			{
+				printf("%d", i);
+				flag++;
+			}
+			else
+				printf("*%d", i);
 			num = num / i;
 		}
 	}
@@ -66,3 +64,8 @@ argv, pero si protegemos el caso en el que se recibe más o menos de un
 argumento, en ese caso imprimimos salto de línea*/
 
 /*Ejemplo) La factorización prima de 96 (2 • 2 • 2 • 2 • 2 • 3)*/
+
+/**
+ * TODO -> Me falla el ejemplo 9539 (no saca salto de linea) y el 1 no imprime
+ * 
+ */
