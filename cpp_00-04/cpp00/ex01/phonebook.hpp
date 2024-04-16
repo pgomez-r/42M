ec535d42feb0cc0d549fc7bef77052d1573ee771
Pedro jesus Gomez ruiz <pgomez-r@c3r6s3.42malaga.com>
2024-04-16 22:42:56 +0200

    16-04-42M

--- cpp_00-04/cpp00/ex01/phonebook.hpp
+++ cpp_00-04/cpp00/ex01/phonebook.hpp
@@ -1,23 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   phonebook.cpp                                      :+:      :+:    :+:   */
+/*   phonebook.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pgomez-r <pgomez-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/15 20:07:25 by pgomez-r          #+#    #+#             */
-/*   Updated: 2024/04/16 21:11:49 by pgomez-r         ###   ########.fr       */
+/*   Created: 2024/04/16 21:12:05 by pgomez-r          #+#    #+#             */
+/*   Updated: 2024/04/16 21:51:09 by pgomez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <cctype>
-
-int	main(int ac, char **av)
+class	Contact
 {
-	if (ac > 1)
-		std::cout << "Usage error" << std::endl;
-	
+	private:
+	public:
+};
 
-	return (0);
-}
+class	PhoneBook
+{
+	private:
+		Contact contact[8];
+	public:	
+		PhoneBook();
+		~PhoneBook();
+};
