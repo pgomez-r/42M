657649db24835cb83d413d410e02cee026a44476
Pedro Gruz <115219064+pgomez-r@users.noreply.github.com>
2024-05-21 04:55:03 +0000

    20-05-BS Subject added

--- cpp_00-04/cpp03/ex00/inc/ClapTrap.hpp
+++ cpp_00-04/cpp03/ex00/inc/ClapTrap.hpp
@@ -3,10 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   ClapTrap.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pgruz11 <pgruz11@student.42.fr>            +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 23:04:05 by pgruz11           #+#    #+#             */
-/*   Updated: 2024/05/18 23:04:06 by pgruz11          ###   ########.fr       */
+/*   Updated: 2024/05/21 04:36:52 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <iostream>
+
+class   ClapTrap{
+	private:
+		std::string	_name;
+		int			_hitPts;
+		int			_energyPts;
+		int			_attackDmg;
+	public:
+		ClapTrap();
+		ClapTrap(std::string name);
+		~ClapTrap();
+		ClapTrap(const ClapTrap &src);
+    	ClapTrap &operator=(const ClapTrap &rhs);
+
+		void	attack(const std::string &target);
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
+};
+
+#endif
--- cpp_00-04/cpp03/ex00/src/ClapTrap.cpp
+++ cpp_00-04/cpp03/ex00/src/ClapTrap.cpp
@@ -3,10 +3,43 @@
 /*                                                        :::      ::::::::   */
 /*   ClapTrap.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pgruz11 <pgruz11@student.42.fr>            +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 23:04:42 by pgruz11           #+#    #+#             */
-/*   Updated: 2024/05/18 23:04:43 by pgruz11          ###   ########.fr       */
+/*   Updated: 2024/05/21 04:36:03 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../inc/ClapTrap.hpp"
+
+ClapTrap(){
+
+}
+
+ClapTrap(std::string name){
+    
+}
+
+~ClapTrap(){
+
+}
+
+ClapTrap(const ClapTrap &src){
+
+}
+
+ClapTrap &operator=(const ClapTrap &rhs){
+    
+}
+
+void	attack(const std::string &target){
+
+}
+
+void	takeDamage(unsigned int amount){
+
+}
+
+void	beRepaired(unsigned int amount){
+
+}
