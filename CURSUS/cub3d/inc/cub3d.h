e8707e468d25fae1d2a9e35187357480b7e52b78
Pedro Gomez Ruiz <pgruz11@Pedros-MacBook-Pro.local>
2024-06-05 02:30:02 +0200

    cub3d_makefile

--- CURSUS/cub3d/inc/cub3d.h
+++ CURSUS/cub3d/inc/cub3d.h
@@ -6,16 +6,15 @@
 /*   By: pgruz11 <pgruz11@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/02 00:13:53 by codespace         #+#    #+#             */
-/*   Updated: 2024/06/02 08:41:33 by pgruz11          ###   ########.fr       */
+/*   Updated: 2024/06/05 00:08:13 by pgruz11          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# include "libft/libft.h"
-# include "MLX42/include/MLX42/MLX42.h"
-# include "MLX42/include/MLX42/MLX42_Input.h"
+# include "./libft/libft.h"
+# include "./MLX42/include/MLX42/MLX42.h"
 
 typedef struct s_mlx_st
 {
