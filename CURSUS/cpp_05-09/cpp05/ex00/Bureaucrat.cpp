b03d18dba4a123c27204bfea640150fd374f7d77
Pedro Gruz <115219064+pgomez-r@users.noreply.github.com>
2024-08-19 05:01:28 +0000

    cpp05-09_init

---
+++ CURSUS/cpp_05-09/cpp05/ex00/Bureaucrat.cpp
@@ -0,0 +1,52 @@
+
+
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(const Bureaucrat &src) : _name(src._name), _grade(src._grade){
+}
+
+Bureaucrat::Bureaucrat(const std::string &name, int grade) : _name(name), _grade(grade)
+{
+	if (grade < 1)
+	{
+		throw Bureaucrat::GradeTooHighException();
+		this->_grade = 150;
+	}
+	if (grade > 150)
+	{
+		throw Bureaucrat::GradeTooLowException();
+		this->_grade = 150;
+	}
+}
+
+Bureaucrat::~Bureaucrat(void){
+}
+
+std::string	Bureaucrat::getName(void)
+{
+	return (this->_name);
+}
+
+int	Bureaucrat::getGrade(void)
+{
+	return (this->_grade);
+}
+
+void	Bureaucrat::gradeUp(void)
+{
+	if (this->_grade - 1 < 1)
+		throw	Bureaucrat::GradeTooHighException();
+	this->_grade--;
+}
+
+void	Bureaucrat::gradeDown(void)
+{
+	if (this->_grade + 1 > 150)
+		throw	Bureaucrat::GradeTooLowException();
+	this->_grade++;
+}
+
+std::ostream	&operator<<(std::ostream &output, const Bureaucrat &rhs)
+{
+	output << rhs.getName() << ", bureaucrat grade " << rhs.getGrade();
+}
---
+++ CURSUS/cpp_05-09/cpp05/ex00/main.cpp
@@ -0,0 +1,8 @@
+
+
+#include "Bureaucrat.hpp"
+
+int main(void)
+{
+	return (0);
+}
