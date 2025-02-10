a8f1eb3e9b458b311a9d22d21d8e824768ae11f9
Pedro jesus Gomez ruiz <pgomez-r@c2r10s4.42malaga.com>
2025-02-10 14:43:39 +0100

    test

--- CURSUS/cpp_05-09/cpp08/ex00/inc/easyfind.hpp
+++ CURSUS/cpp_05-09/cpp08/ex00/inc/easyfind.hpp
@@ -6,13 +6,39 @@
 /*   By: pgomez-r <pgomez-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 13:16:31 by pgomez-r          #+#    #+#             */
-/*   Updated: 2025/02/07 16:52:59 by pgomez-r         ###   ########.fr       */
+/*   Updated: 2025/02/10 14:43:14 by pgomez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <iterator>
+
+class	notFound : public std::exception
+{
+	public:
+		const char *what() const throw()
+		{
+			return ("Element not found in the container");
+		}
+};
+
+template <typename T>
+void	easyFind(T &cont, int n)
+{
+	typename T::iterator	i = std::find(cont.begin(), cont.end(), n);
+	if (std::find(cont.begin(), cont.end(), n) != cont.end())
+	{
+		int	pos = std::distance(cont.begin(), i);
+		std::cout << n << " value found in the container at index " << pos << std::endl;
+	}
+	else
+		throw (notFound());
+}
 
 
 #endif
--- CURSUS/cpp_05-09/cpp08/ex00/src/main.cpp
+++ CURSUS/cpp_05-09/cpp08/ex00/src/main.cpp
@@ -6,11 +6,67 @@
 /*   By: pgomez-r <pgomez-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 13:16:27 by pgomez-r          #+#    #+#             */
-/*   Updated: 2025/02/07 17:00:21 by pgomez-r         ###   ########.fr       */
+/*   Updated: 2025/02/10 14:40:55 by pgomez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(void)
+#include "easyfind.hpp"
+#include <vector>
+#include <list>
+#include <deque>
+#include <set>
+
+int main()
 {
-	return (0);
+	// Test with std::vector
+	std::vector<int> vec;
+	vec.push_back(1);
+	vec.push_back(2);
+	vec.push_back(3);
+	vec.push_back(4);
+	vec.push_back(5);
+	try
+	{
+		easyFind(vec, 3);
+		easyFind(vec, 6);
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	// Test with std::list
+	std::list<int> lst;
+	lst.push_back(10);
+	lst.push_back(20);
+	lst.push_back(30);
+	lst.push_back(40);
+	lst.push_back(50);
+	try
+	{
+		easyFind(lst, 20);
+		easyFind(lst, 60);
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	// Test with std::deque
+	std::deque<int> deq;
+	deq.push_back(100);
+	deq.push_back(200);
+	deq.push_back(300);
+	deq.push_back(400);
+	deq.push_back(500);
+	try
+	{
+		easyFind(deq, 300);
+		easyFind(deq, 600);
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	return 0;
 }
